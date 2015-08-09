72debc31ec18752f7c51b0b0d8219c927a3a821a
March Liu <march.liu@gmail.com>
2015-08-09 16:38:34 +0800

    add byte parsecs

---
+++ byte.go
@@ -0,0 +1,110 @@
+package goparsec2
+
+import "fmt"
+
+// Byte 判断下一个字节是否与给定值相等
+func Byte(val byte) Parsec {
+	return Parsec{func(state State) (interface{}, error) {
+		x, err := state.Next()
+		fmt.Printf("expect %c    get %c \n", val, x)
+		if err != nil {
+			return nil, err
+		}
+		if c, ok := x.(byte); ok {
+			if c == val {
+				return c, nil
+			}
+			return nil, state.Trap("Expect '%v' but '%v'", string([]byte{val}), string([]byte{c}))
+		}
+		return nil, state.Trap("Expect a byte '%s' but x is %v", string([]byte{val}), x)
+	}}
+}
+
+// NByte 判断下一个字符是否与给定值不相等
+func NByte(val byte) Parsec {
+	return Parsec{func(state State) (interface{}, error) {
+		x, err := state.Next()
+		if err != nil {
+			return nil, err
+		}
+		if c, ok := x.(byte); ok {
+			if c == val {
+				return nil, state.Trap("Expect not '%v' but '%v'", string([]byte{val}), string([]byte{c}))
+			}
+			return c, nil
+		}
+		return nil, state.Trap("Expect a rune '%s' but x is %v", string([]byte{val}), x)
+	}}
+}
+
+// ByteOf 检查后续的字符是否是给定值中的某一个
+func ByteOf(str string) Parsec {
+	data := []byte(str)
+	return Parsec{func(state State) (interface{}, error) {
+		x, err := state.Next()
+		if err != nil {
+			return nil, err
+		}
+		if c, ok := x.(byte); ok {
+			for _, r := range data {
+				if c == r {
+					return c, nil
+				}
+			}
+			return nil, state.Trap("Expect rune in '%s' but '%s'", str, string([]byte{c}))
+		}
+		return nil, state.Trap("Expect rune in '%s' but x=%v is %t", str, x, x)
+	}}
+}
+
+// ByteNone 检查后续的字符是否不是给定值中的任一个
+func ByteNone(str string) Parsec {
+	data := []byte(str)
+	return Parsec{func(state State) (interface{}, error) {
+		x, err := state.Next()
+		if err != nil {
+			return nil, err
+		}
+		if c, ok := x.(byte); ok {
+			for _, r := range data {
+				if c == r {
+					return nil, state.Trap("Expect rune none of '%s' but '%s'", str, string([]byte{c}))
+				}
+			}
+			return c, nil
+		}
+		return nil, state.Trap("Expect rune none of '%s' but x=%v is %t", str, x, x)
+	}}
+}
+
+// Bytes 判断后续的字节串是否匹配给定的串
+func Bytes(str string) Parsec {
+	data := []byte(str)
+	return Parsec{func(state State) (interface{}, error) {
+		for _, r := range data {
+			_, err := Byte(r).Parse(state)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return str, nil
+	}}
+}
+
+// ByteParsec 通过一个谓词参数，提供通用的 rune 算子生成判断
+func ByteParsec(name string, pred func(r byte) bool) Parsec {
+	return Parsec{func(state State) (interface{}, error) {
+		x, err := state.Next()
+		if err != nil {
+			return nil, err
+		}
+		if c, ok := x.(byte); ok {
+			r := byte(c)
+			if pred(r) {
+				return c, nil
+			}
+			return nil, state.Trap("Expect %s but '%v'", name, string([]byte{r}))
+		}
+		return nil, state.Trap("Expect %s but x=%v is %t", name, x, x)
+	}}
+}
